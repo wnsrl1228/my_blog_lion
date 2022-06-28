a946f14d57e73222c87e6705d80dbfdbded1f5ad
wnsrl1228 <wnsrl981228@gmail.com>
2022-06-28 15:35:45 +0900

    블로그

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import React from "react";
 
 import {
@@ -7,12 +7,26 @@ import {
     navLinks,
     navLinkItem,
     navLinkText,
-  } from "./layout.module.css";
+    siteTitle,
+  } from "./Layout.module.css";
   
   function Layout({ pageTitle, children }) {
+    const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
     return (
       <div className={container}>
-        <title>{pageTitle}</title>
+        <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+        </title>
+        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
         <nav>
           <ul className={navLinks}>
             <li className={navLinkItem}>
@@ -23,6 +37,11 @@ import {
             <li className={navLinkItem}>
               <Link className={navLinkText} to="/about">
                 About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/blog">
+                Blog
               </Link>
             </li>
           </ul>
